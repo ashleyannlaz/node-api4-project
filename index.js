449dab0b8896f146a914dc6039c481c768d43721
ashleyannlaz <paperpastels@gmail.com>
2021-09-02 17:32:04 -0400

    Added requests

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
-// server.get('/api/users', (req, res) => {
-//     res.json({ message: 'Get Users' })
-// })
-
-// server.post('/api/register', (req, res) => {
-//     res.json({ message: 'Register' })
-
-// })
-
-// server.post('/api/login', (req, res) => {
-//     res.json({ message: 'Login' })
-
-// })
-
 server.get('/', (req, res) => {
     res.send(`<h1>Hello!</h1>`)
   })
+
+server.get('/api/users', (req, res) => {
+    res.json({ message: 'Get Users' })
+})
+
+server.post('/api/register', (req, res) => {
+    res.json({ message: 'Register' })
+
+})
+
+server.post('/api/login', (req, res) => {
+    res.json({ message: 'Login' })
+
+})
 
 server.listen(PORT, () => {
     console.log(`*Listening on ${PORT}*`);
